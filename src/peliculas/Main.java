2687ca3bf3aab74b566d880e7f6e6cdd8a980616
diego <diegoonate3026@gmail.com>
2023-06-26 22:16:28 -0500

    interfaz de usuario

--- src/peliculas/Main.java
+++ src/peliculas/Main.java
@@ -1,28 +1,11 @@
 package peliculas;
 
-import javax.swing.JOptionPane;
+import peliculas.ui.UIPeliculas;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*JOptionPane.ERROR_MESSAGE. -> 0
-        * JOptionPane.INFORMATION_MESSAGE. -> 1
-        * JOptionPane.WARNING_MESSAGE. -> 2
-        * JOptionPane.QUESTION_MESSAGE. -> 3
-         */
-
-        String valor = JOptionPane.showInputDialog(
-                null,
-                "Ingres texto",
-                "Entrada",
-                3);
-
-        JOptionPane.showMessageDialog(
-                null,
-                valor,
-                "Bienvenida",
-                JOptionPane.INFORMATION_MESSAGE);
-
+        UIPeliculas.interfazUsuario();
     }
 
 }
---
+++ src/peliculas/ui/UIPeliculas.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package peliculas.ui;
+
+import javax.swing.JOptionPane;
+import peliculas.modelo.ICatalogoPeliculas;
+import peliculas.modelo.ImplementacionCatalogoPeliculas;
+import peliculas.modelo.Pelicula;
+
+/**
+ *
+ * @author asus
+ */
+public class UIPeliculas {
+    public static void interfazUsuario(){
+        ICatalogoPeliculas peliculas = new ImplementacionCatalogoPeliculas();
+        Pelicula pelicula;
+        
+        cerrar: 
+        while(true){
+            String opcion = JOptionPane.showInputDialog(
+                null,
+                "1 - Insertar pelicula\n"
+                + "2 - Listar pelicula\n"
+                + "3 - Buscar pelicula\n"
+                + "4 - Salir",
+                "Ingrese una opcion",
+                JOptionPane.QUESTION_MESSAGE
+            );
+            
+            switch(opcion){
+                case "1":
+                    String nombrePelicula = JOptionPane.showInputDialog(
+                            null,
+                            "INGRESE EL NOMBRE DE LA PELICULA",
+                            "entrada",
+                            3);
+                    
+                    pelicula = new Pelicula(nombrePelicula);
+                    peliculas.insertarPeliculas(pelicula);
+                    break;
+                case "2":
+                    peliculas.listarPeliculas();
+                    break;
+                case "3":
+                    nombrePelicula = JOptionPane.showInputDialog(
+                            null,
+                            "INGRESE EL NOMBRE DE LA PELICULA",
+                            "Ingrese",
+                            3);
+                    peliculas.buscarPelicula(nombrePelicula);
+                    break;
+                case "4":
+                    break cerrar;
+                default:
+                    JOptionPane.showMessageDialog(null, 
+                            "OPCION INCORRECTA \n"
+                            + "VUELVE A INGRESAR UNA \n"
+                            + "OPCION CORRECTA \n"
+                            + "LAS OPCIONES SON DEL 1 AL 4",
+                            "ERROR",
+                            JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+}
